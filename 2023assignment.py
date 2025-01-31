673a849da0283b3199310eb47ccf4fac33380f6a
supremeneupane43 <supremeneupane43@gmail.com>
2025-01-31 12:53:20 +0000

    hello

--- 2023assignment.py
+++ 2023assignment.py
@@ -4,29 +4,28 @@ journeyStage = ["A","B","C"]
 costjourneystage = []
 startMiles = int(input("What is the mileage at the start of the journey?: "))
 nochargingStations = int(input("How many charging stations were visited"))
-for x in range(nochargingStations):
+for index in range(nochargingStations):
     currentMiles = int(input("Enter current mileage: "))
     kwrating = int(input("Enter kw rating"))
-    while kwrating != 7 and !=22 and !=50:
+    while kwrating != 7 and kwrating != 22 and kwrating !=50:
         print("Please input a valid kw rating")
         kwrating = int(input("Enter kw rating"))
     if kwrating == 7:
-        pricePerMile = 0
+        pricePerMile = 3
     elif kwrating == 22:
-        pricePerMile == 0.005
+        pricePerMile = 0.005
     else:
         pricePerMile = 0.01
     milesTravelled = currentMiles - startMiles
     startMiles = currentMiles
-    journeystage = pricePerMile * milesTravelled
+    journeystage = float(pricePerMile * milesTravelled)
     costjourneystage.append(journeystage)
-    
-for x in range(nochargingStations):
+for index in range(nochargingStations):
     print(journeyStage[index])
-    print(costjourneystage)
+    print(costjourneystage[index])
     totaljourneycost += costjourneystage[index]
     totalMiles += milesTravelled
 print(round(totaljourneycost, 2))
-print("You have travelled", totalMiles)
+print("You have travelled", totalMiles, "miles")
     
 
